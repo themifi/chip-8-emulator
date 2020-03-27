ee67b4ea4a16dccae8f26749f25847728f657388
Michael Rybakov <me@opilar.com>
2020-03-27 11:11:37 +0300

    Input::is_key_pressed

--- src/input.rs
+++ src/input.rs
@@ -1,3 +1,5 @@
+const KEYS: u8 = 16;
+
 pub struct Input {
     keypad: u16,
 }
@@ -7,5 +9,54 @@ impl Input {
         Self {
             keypad: 0,
         }
+    }
+
+    pub fn new_with_state(keypad: u16) -> Self {
+        Self {
+            keypad,
+        }
+    }
+
+    pub fn is_key_pressed(&self, key: u8) -> bool {
+        assert!(key < KEYS);
+        ((1 << key) & self.keypad) != 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_key_pressed_clear_state() {
+        let input = Input::new();
+        for key in 0..KEYS {
+            assert!(!input.is_key_pressed(key));
+        }
+    }
+
+    #[test]
+    fn test_is_key_pressed_with_key_pressed() {
+        for pressed_key in 0..KEYS {
+            let input = Input::new_with_state(1 << pressed_key);
+            for key in 0..KEYS {
+                assert_eq!(input.is_key_pressed(key), pressed_key == key);
+            }
+        }
+    }
+
+    #[test]
+    fn test_is_key_pressed_with_all_keys_pressed() {
+        let input = Input::new_with_state(0xFFFF);
+        for key in 0..KEYS {
+            assert!(input.is_key_pressed(key));
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_is_key_pressed_invalid_input() {
+        let input = Input::new();
+        input.is_key_pressed(KEYS);
     }
 }
